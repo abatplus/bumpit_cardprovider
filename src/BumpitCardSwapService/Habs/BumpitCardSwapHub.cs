a05792a22d2c270f6bbe3cf91a65631ef04848d6
izhlobich <63245560+izhlobich@users.noreply.github.com>
2020-07-13 16:33:28 +0200

    Add BumpitCardSwapService

---
+++ src/BumpitCardSwapService/Habs/BumpitCardSwapHub.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BumpitCardSwapService
+{
+    public class BumpitCardSwapHub : Hub<IBumpitCardSwapHub>
+    {
+        async Task SubcribeToSwap(SubscriptionData subsData)
+        {
+            //await Groups.AddToGroupAsync(Context.ConnectionId, boardId.ToString());
+
+            List<JObject> list = new List<JObject>();
+
+            await Clients.Caller.Subscribed(list);
+        }
+
+        async Task UnSubcribeFromSwap(SubscriptionData subsData)
+        {
+            List<JObject> list = new List<JObject>();
+
+            await Clients.Caller.UnSubscribed(list);
+        }
+
+        async Task UpdateGeolocationData(string deviceId, double longitude, double latitude)
+        {
+            List<JObject> list = new List<JObject>();
+
+            await Clients.Caller.GeolocationDataUpdated(list);
+        }
+
+        async Task UpdateSubscriptionData(string deviceId, string firstName, string lastName)
+        {
+            List<JObject> list = new List<JObject>();
+
+            await Clients.Caller.SubscriptionDataUpdated(list);
+        }
+
+        async Task StartExchangeRequest(string deviceIdSender, string deviceIdRecipient)
+        {
+
+            //TODO
+            await Clients.Caller.ReceivedExchangeRequest(new SubscriptionData());
+        }
+
+        async Task AcceptExchangeRequest(string deviceIdSender, string deviceIdRecipient, string cardDataSender)
+        {
+            //TODO
+            await Clients.Caller.ReceivedCardData(String.Empty, String.Empty);
+        }
+
+    }
+}
---
+++ src/BumpitCardSwapService/Habs/IBumpitCardSwapHub.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace BumpitCardSwapService
+{
+    public interface IBumpitCardSwapHub
+    {
+        Task Subscribed(IEnumerable<JObject> subscribedDevices);
+
+        Task UnSubscribed(IEnumerable<JObject> subscribedDevices);
+
+        Task GeolocationDataUpdated(IEnumerable<JObject> subscribedDevices);
+
+        Task SubscriptionDataUpdated(IEnumerable<JObject> subscribedDevices);
+        
+        Task ReceivedExchangeRequest(SubscriptionData senderData);
+
+        Task ReceivedCardData(string idSender, string cardDataSender);
+    }
+}
