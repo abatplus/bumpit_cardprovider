66ba56e2e832f7cf8bf836fdccbcd6cc4e501e38
izhlobich <63245560+izhlobich@users.noreply.github.com>
2020-07-23 12:41:46 +0200

    Some unit tests for hub

--- src/CardExchangeServiceTests/CardExchangeHubTest.cs
+++ src/CardExchangeServiceTests/CardExchangeHubTest.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
 using Xunit;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 using FluentAssertions;
 using System.Threading.Tasks;
@@ -24,118 +25,266 @@ namespace CardExchangeServiceTests
         private const double latitudeIn2 = 12.466561156;
         private const double longitude = -34.405804850;
 
-        private const string ConnectionId1 = "81sQRSCbfEidBxsIO9bnyQ";
-        private const string ConnectionId2 = "81sQRSCbfEidBxsIO9bny1";
-        private const string ConnectionId3 = "81sQRSCbfEidBxsIO9bny2";
-
-
-        //private readonly Mock<IElasticClient> elasticClientMock;
-        private readonly Mock<IConfiguration> configurationMock;
-        private ICardExchangeHub cardExchangeHub;
-        private MockCardExchangeClient cardExchangeClient;
-        private readonly Mock<ISubscriptionDataRepository> repository;
-
-        private Mock<IHubCallerClients<ICardExchangeClient>> mockClients;
-        private Mock<HubCallerContext> mockContext;
-        private Mock<IGroupManager> mockGroups;
-
-        private readonly IRedisClient _redisClient;
-        private readonly ISubscriptionDataRepository _repository;
-
         public CardExchangeHubTest()
         {
-            configurationMock = new Mock<IConfiguration>();
-
-            configurationMock.Setup(x => x["Redis:Host"]).Returns("localhost");
-            configurationMock.Setup(x => x["Redis:Port"]).Returns("6379");
-            configurationMock.Setup(x => x["Redis:GeoRadius_m"]).Returns("5");
-            configurationMock.Setup(x => x["Redis:KeyExpireTimeout_s"]).Returns("2");
-
-            repository = new Mock<ISubscriptionDataRepository>();
-            mockClients = new Mock<IHubCallerClients<ICardExchangeClient>>();
-            mockGroups = new Mock<IGroupManager>();
-            mockContext = new Mock<HubCallerContext>();
-            cardExchangeClient = new MockCardExchangeClient();
-            mockClients.Setup(x => x.Caller).Returns(cardExchangeClient);
-
-            _redisClient = new RedisClient(configurationMock.Object);
-            _repository = new SubscriptionDataRepository(_redisClient);
-
-            //            cardExchangeHub = new CardExchangeHub(repository.Object);
-            cardExchangeHub = new CardExchangeHub(_repository);
-            ((Hub<ICardExchangeClient>)cardExchangeHub).Clients = mockClients.Object;
-            ((Hub<ICardExchangeClient>)cardExchangeHub).Groups = mockGroups.Object;
-            ((Hub<ICardExchangeClient>)cardExchangeHub).Context = mockContext.Object;
         }
 
         [Fact]
-        public async void CardExchangeHubTest_Subscribe_SubscribedCalled()
+        public async void ConnectionTest_SubscribeUpdate2Subscribers_SubscribedCalled()
         {
-            var connection = new HubConnectionBuilder()
+            var connection1 = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/swaphub")
+                .Build();
+            var connection2 = new HubConnectionBuilder()
                 .WithUrl("http://localhost:5000/swaphub")
                 .Build();
 
-            mockContext.Setup(x => x.ConnectionId).Returns(ConnectionId1);
+            bool isSubscribedCalled1 = false;
+            IEnumerable<string> resPeers1 = new List<string>();
+            connection1.On(nameof(ICardExchangeClient.Subscribed), (IEnumerable<string> peers) =>
+            {
+                isSubscribedCalled1 = true;
+                resPeers1 = peers;
+            });
+            bool isUpdatedCalled1 = false;
+            connection1.On(nameof(ICardExchangeClient.Updated), (IEnumerable<string> peers) =>
+            {
+                isUpdatedCalled1 = true;
+                resPeers1 = peers;
+            });
 
-            await connection.StartAsync().ContinueWith(x =>
-           {
-               if (connection.State == HubConnectionState.Connected)
-               {
-                   cardExchangeHub.Subscribe(deviceId1, longitude, latitude1, "displayName1");
-               }
-           });
+            bool isSubscribedCalled2 = false;
+            IEnumerable<string> resPeers2 = new List<string>();
+            connection2.On(nameof(ICardExchangeClient.Subscribed), (IEnumerable<string> peers) =>
+            {
+                isSubscribedCalled2 = true;
+                resPeers2 = peers;
+            });
+
+
+            bool isUpdatedCalled2 = false;
+            connection2.On(nameof(ICardExchangeClient.Updated), (IEnumerable<string> peers) =>
+            {
+                isUpdatedCalled2 = true;
+                resPeers2 = peers;
+            });
+
+            await connection1.StartAsync().ContinueWith(x =>
+            {
+                if (connection1.State == HubConnectionState.Connected)
+                {
+                    connection1.SendAsync("Subscribe", deviceId1, longitude, latitude1, "displayName1");
+                }
+            });
+
+            await connection2.StartAsync().ContinueWith(x =>
+            {
+                if (connection2.State == HubConnectionState.Connected)
+                {
+                    connection2.SendAsync("Subscribe", deviceId2, longitude, latitudeIn2, "displayName2");
+
+                }
+            });
+
+            await connection1.SendAsync("Update", deviceId1, longitude, latitude1, "displayName1");
+            await connection2.SendAsync("Update", deviceId2, longitude, latitudeIn2, "displayName2");
 
             await Task.Delay(2000);
 
-            cardExchangeClient.Peers.Should().NotBeNull();
+            isSubscribedCalled1.Should().BeTrue();
+            isUpdatedCalled1.Should().BeTrue();
+            resPeers1.Should().NotBeNull();
+            resPeers1.Count().Should().Be(1);
+            var data1 = JsonConvert.DeserializeObject<SubscriptionData>(((List<string>)resPeers1)[0]);
+            data1.Should().NotBeNull();
+            data1.Latitude.Should().Be(0);
+            data1.Longitute.Should().Be(0);
+            data1.DeviceId.Should().Be(deviceId2);
+            data1.DisplayName.Should().Be("displayName2");
+
+
+            isSubscribedCalled2.Should().BeTrue();
+            isUpdatedCalled2.Should().BeTrue();
+            resPeers2.Should().NotBeNull();
+            var data2 = JsonConvert.DeserializeObject<SubscriptionData>(((List<string>)resPeers2)[0]);
+            data2.Should().NotBeNull();
+            data2.Latitude.Should().Be(0);
+            data2.Longitute.Should().Be(0);
+            data2.DeviceId.Should().Be(deviceId1);
+            data2.DisplayName.Should().Be("displayName1");
         }
 
-
         [Fact]
-        public async void CardExchangeHubTest_TwoSubscribers_SubscribedCalled()
+        public async void ConnectionTest_Subscribe_SubscribedCalled()
         {
             var connection = new HubConnectionBuilder()
                 .WithUrl("http://localhost:5000/swaphub")
                 .Build();
 
-            CardExchangeHub hub1, hub2;
-            MockCardExchangeClient client1, client2;
-
-            SubscribeOneClient(ConnectionId1, out hub1, out client1);
-            SubscribeOneClient(ConnectionId2, out hub2, out client2);
+            bool isSubscribedCalled = false;
+            IEnumerable<string> resPeers = new List<string>();
+            connection.On(nameof(ICardExchangeClient.Subscribed), (IEnumerable<string> peers) =>
+            {
+                isSubscribedCalled = true;
+                resPeers = peers;
+            });
 
             await connection.StartAsync().ContinueWith(x =>
             {
                 if (connection.State == HubConnectionState.Connected)
                 {
-                    hub1.Subscribe(deviceId1, longitude, latitude1, "displayName1");
-                    hub2.Subscribe(deviceId2, longitude, latitudeIn2, "displayName2");
+                    connection.SendAsync("Subscribe", deviceId1, longitude, latitude1, "displayName1");
                 }
             });
 
             await Task.Delay(2000);
 
-            client1.Peers.Should().NotBeNull();
-            client2.Peers.Should().NotBeNull();
+            isSubscribedCalled.Should().BeTrue();
+            resPeers.Should().NotBeNull();
+            resPeers.Count().Should().Be(0);
         }
-
-        private void SubscribeOneClient(string ConnectionId, out CardExchangeHub cardExchangeHub, out MockCardExchangeClient cardExchangeClient)
+        
+        [Fact]
+        public async void ConnectionTest_UnSubscribe_UnSubscribedCalled()
         {
-            var mockClients = new Mock<IHubCallerClients<ICardExchangeClient>>();
-            var mockGroups = new Mock<IGroupManager>();
-            var mockContext = new Mock<HubCallerContext>();
-            cardExchangeClient = new MockCardExchangeClient();
-            mockClients.Setup(x => x.Caller).Returns(cardExchangeClient);
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/swaphub")
+                .Build();
 
-            cardExchangeHub = new CardExchangeHub(_repository);
-            cardExchangeHub.Clients = mockClients.Object;
-            cardExchangeHub.Groups = mockGroups.Object;
-            cardExchangeHub.Context = mockContext.Object;
+            bool isSubscribedCalled = false;
+            IEnumerable<string> resPeers = new List<string>();
+            connection.On(nameof(ICardExchangeClient.Subscribed), (IEnumerable<string> peers) =>
+            {
+                isSubscribedCalled = true;
+                resPeers = peers;
+            });
 
-            mockContext.Setup(x => x.ConnectionId).Returns(ConnectionId);
+            bool isUnSubscribedCalled = false;
+            string statusMessageUnSubscribed = String.Empty;
+            connection.On(nameof(ICardExchangeClient.Unsubscribed), (string statusMessage) =>
+            {
+                isUnSubscribedCalled = true;
+                statusMessageUnSubscribed = statusMessage;
+            });
 
-            var caller = new MockCardExchangeClient();
-            mockClients.Setup(x => x.Caller).Returns(caller);
+            await connection.StartAsync().ContinueWith(async x =>
+            {
+                if (connection.State == HubConnectionState.Connected)
+                {
+                    await connection.SendAsync("Subscribe", deviceId1, longitude, latitude1, "displayName1");
+                }
+            });
+
+            await Task.Delay(2000);
+
+            isSubscribedCalled.Should().BeTrue();
+            resPeers.Should().NotBeNull();
+            resPeers.Count().Should().Be(0);
+
+            await connection.SendAsync("Unsubscribe", deviceId1);
+
+            await Task.Delay(2000);
+
+            isUnSubscribedCalled.Should().BeTrue();
+            statusMessageUnSubscribed.Should().NotBeNullOrWhiteSpace();
         }
+        
+        [Fact]
+        public async void ConnectionTest_Subscribe2Subscribers_ManyUpdates_NoFallOuts()
+        {
+            var connection1 = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/swaphub")
+                .Build();
+            var connection2 = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/swaphub")
+                .Build();
+
+            bool isSubscribedCalled1 = false;
+            IEnumerable<string> resPeers1 = new List<string>();
+            connection1.On(nameof(ICardExchangeClient.Subscribed), (IEnumerable<string> peers) =>
+            {
+                   isSubscribedCalled1 = true;
+                resPeers1 = peers;
+            });
+            bool isUpdatedCalled1 = false;
+            int iCallUpdate1 = 0;
+            connection1.On(nameof(ICardExchangeClient.Updated), (IEnumerable<string> peers) =>
+            {
+                iCallUpdate1++;
+                isUpdatedCalled1 = true;
+                resPeers1 = peers;
+            });
+
+            bool isSubscribedCalled2 = false;
+            IEnumerable<string> resPeers2 = new List<string>();
+            connection2.On(nameof(ICardExchangeClient.Subscribed), (IEnumerable<string> peers) =>
+            {
+                isSubscribedCalled2 = true;
+                resPeers2 = peers;
+            });
+
+
+            bool isUpdatedCalled2 = false;
+            int iCallUpdate2 = 0;
+            connection2.On(nameof(ICardExchangeClient.Updated), (IEnumerable<string> peers) =>
+            {
+                iCallUpdate2++;
+                isUpdatedCalled2 = true;
+                resPeers2 = peers;
+            });
+
+            await connection1.StartAsync().ContinueWith(x =>
+            {
+                if (connection1.State == HubConnectionState.Connected)
+                {
+                    connection1.SendAsync("Subscribe", deviceId1, longitude, latitude1, "displayName1");
+                }
+            });
+
+            await connection2.StartAsync().ContinueWith(x =>
+            {
+                if (connection2.State == HubConnectionState.Connected)
+                {
+                    connection2.SendAsync("Subscribe", deviceId2, longitude, latitudeIn2, "displayName2");
+
+                }
+            });
+
+            for (int i = 0; i < 50; i++)
+            {
+                connection1.SendAsync("Update", deviceId1, longitude, latitude1, "displayName1");
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                connection2.SendAsync("Update", deviceId2, longitude, latitudeIn2, "displayName2");
+            }
+
+            await Task.Delay(2000);
+
+            iCallUpdate1.Should().Be(50);
+            isSubscribedCalled1.Should().BeTrue();
+            isUpdatedCalled1.Should().BeTrue();
+            resPeers1.Should().NotBeNull();
+            resPeers1.Count().Should().Be(1);
+            var data1 = JsonConvert.DeserializeObject<SubscriptionData>(((List<string>)resPeers1)[0]);
+            data1.Should().NotBeNull();
+            data1.Latitude.Should().Be(0);
+            data1.Longitute.Should().Be(0);
+            data1.DeviceId.Should().Be(deviceId2);
+            data1.DisplayName.Should().Be("displayName2");
+
+            iCallUpdate2.Should().Be(50);
+            isSubscribedCalled2.Should().BeTrue();
+            isUpdatedCalled2.Should().BeTrue();
+            resPeers2.Should().NotBeNull();
+            var data2 = JsonConvert.DeserializeObject<SubscriptionData>(((List<string>)resPeers2)[0]);
+            data2.Should().NotBeNull();
+            data2.Latitude.Should().Be(0);
+            data2.Longitute.Should().Be(0);
+            data2.DeviceId.Should().Be(deviceId1);
+            data2.DisplayName.Should().Be("displayName1");
+        }
+
     }
 }
