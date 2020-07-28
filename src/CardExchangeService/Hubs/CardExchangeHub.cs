5008fb07655aa4d383a7d449cc5096bf1d0f8c77
izhlobich <63245560+izhlobich@users.noreply.github.com>
2020-07-28 17:33:22 +0200

    Correct interfaces to support images

--- src/CardExchangeService/Hubs/CardExchangeHub.cs
+++ src/CardExchangeService/Hubs/CardExchangeHub.cs
@@ -13,7 +13,7 @@ namespace CardExchangeService
             _repository = repository;
         }
 
-        public async Task Subscribe(string deviceId, double longitude, double latitude, string displayName)
+        public async Task Subscribe(string deviceId, double longitude, double latitude, string displayName, string image)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, deviceId)
             .ContinueWith(async _ => await _repository.SaveSubscriber(deviceId, longitude, latitude, displayName))
@@ -35,7 +35,9 @@ namespace CardExchangeService
 
         public async Task RequestCardExchange(string deviceId, string peerDeviceId, string displayName)
         {
-            await Clients.Group(peerDeviceId).CardExchangeRequested(deviceId, displayName)
+            string thumbnailUrl = string.Empty;
+            //TODO : get thumbnailUrl
+            await Clients.Group(peerDeviceId).CardExchangeRequested(deviceId, displayName, thumbnailUrl)
             .ContinueWith(_ => Clients.Caller.WaitingForAcceptance(peerDeviceId));
         }
         public async Task RevokeCardExchangeRequest(string deviceId, string peerDeviceId)
@@ -46,13 +48,17 @@ namespace CardExchangeService
 
         public async Task AcceptCardExchange(string deviceId, string peerDeviceId, string peerDisplayName, string peerCardData)
         {
-            await Clients.Group(deviceId).CardExchangeAccepted(peerDeviceId, peerDisplayName, peerCardData)
+            string peerImage = string.Empty;
+            //TODO : get peerImage
+            await Clients.Group(deviceId).CardExchangeAccepted(peerDeviceId, peerDisplayName, peerCardData, peerImage)
             .ContinueWith(_ => Clients.Caller.AcceptanceSent(deviceId));
         }
 
         public async Task SendCardData(string deviceId, string peerDeviceId, string displayName, string cardData)
         {
-            await Clients.Group(peerDeviceId).CardDataReceived(deviceId, displayName, cardData)
+            string image = string.Empty;
+            //TODO : get image
+            await Clients.Group(peerDeviceId).CardDataReceived(deviceId, displayName, cardData, image)
             .ContinueWith(_ => Clients.Caller.CardDataSent(peerDeviceId));
         }
     }
