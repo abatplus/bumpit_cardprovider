a7cf232aa1d880df6fc6038a1ecaae77425c3496
Michael Theobald <michael.theobald@abatplus.de>
2020-08-05 07:13:18 +0200

    added delay timer

---
+++ src/CardExchangeServiceTests/DelayTimerTest.cs
@@ -0,0 +1,79 @@
+using System.Threading;
+using System;
+using Xunit;
+using CardExchangeService;
+using FluentAssertions;
+
+namespace CardExchangeServiceTests
+{
+    public class DelayTimerTest
+    {
+        string _savedMessage;
+
+        private DelayTimer CreateTimer() => new DelayTimer(state => _savedMessage = state as string, "INIT", 100);
+
+        [Fact]
+        public void TestDirectInvocation()
+        {
+            using(DelayTimer dt = CreateTimer())
+            {
+                dt.InvokeDirect();
+                
+                _savedMessage.Should().Be("INIT");
+            }
+        }
+
+        [Fact]
+        public void TestSingleInvocation()
+        {
+            using(DelayTimer dt = CreateTimer())
+            {
+                dt.Invoke("SINGLE");
+
+                Thread.Sleep(110);
+                
+                _savedMessage.Should().Be("SINGLE");
+            }
+        }
+
+        [Fact]
+        public void TestMultipleInvocation()
+        {
+            using(DelayTimer dt = CreateTimer())
+            {            
+                dt.Invoke("ONE-FAILURE!");
+                Thread.Sleep(50);
+                dt.Invoke("TWO-FAILURE!");
+                Thread.Sleep(50);
+                dt.Invoke("THREE");
+                
+                Thread.Sleep(110);  
+                
+                _savedMessage.Should().Be("THREE");
+                
+                dt.Invoke();
+                
+                Thread.Sleep(110);
+
+                _savedMessage.Should().Be("INIT");
+            }
+        }
+
+        [Fact]
+        public void TestStop()
+        {
+            _savedMessage = "";
+
+            using(DelayTimer dt = CreateTimer())
+            {                
+                dt.Invoke("STOP-FAILURE!");           
+                Thread.Sleep(50);
+                dt.Stop();
+                
+                Thread.Sleep(110); 
+            } 
+            
+            _savedMessage.Should().Be("");
+        }
+    }
+}
